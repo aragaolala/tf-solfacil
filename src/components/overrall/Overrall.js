883215ebcb8f8c42bd74cfe9af82148d12b44183
loulima <louizalima13@gmail.com>
2022-06-28 16:53:28 -0300

    tabela completa + box somatoria

--- src/components/overrall/Overrall.js
+++ src/components/overrall/Overrall.js
@@ -7,9 +7,11 @@ export function Overrall ({
     return(
         <>
         <div className="sumContainer">
-        <h3 className="sumTitle"> Valor {value} </h3>
+            <h3 className="sumTitle"> Valor {value} </h3>
+            <div className="totalSumContainer">
                 <div className="totalSum">   PF R${value}</div>
                 <div className="totalSum">   PJ R${value}</div>
+        </div>  
             
         <p className="GlobalSumResult">  Total R$ {value} </p>
         </div>
