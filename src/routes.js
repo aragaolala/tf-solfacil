2a31e45b3677de766eddd6277efe91a3ecf446b8
loulima <louizalima13@gmail.com>
2022-06-28 15:58:30 -0300

    tabela PRONTA ^_^

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Pages/Login/login";
+import Login from "./pages/Login/login";
 import Feed from "../src/feed";
 
 // import { PrivateRoute } from "./privateRoute";
