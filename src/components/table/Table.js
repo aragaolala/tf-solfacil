767ceeaad7721673afde23312c46564f5ec25471
loulima <louizalima13@gmail.com>
2022-06-29 07:52:31 -0300

    Valores do Corpo da tabela aparecendo na tela

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -8,13 +8,13 @@ export function Table() {
   return (
     <>
       <table className="table">
-        <TableHead value= {['Nome do Cliente', 'Valor Financiado', 'Valor Bruto', 'Valor de aquisição','Prazo','Nº CCB']} />
+        <TableHead value= {['', '', '', '','',' ']} />
         
-        <TableBody value= {['Fulano', 'R$40.000,00', 'R$40.000,00', 'R$40.000,00','48x','124124214']} />
-        <TableBody value= {['Fulano', 'R$40.000,00', 'R$40.000,00', 'R$40.000,00','48x','124124214']} />
-        <TableBody value= {['Fulano', 'R$40.000,00', 'R$40.000,00', 'R$40.000,00','48x','124124214']} />
-        <TableBody value= {['Fulano', 'R$40.000,00', 'R$40.000,00', 'R$40.000,00','48x','124124214']} />
-        <TableBody value= {['Fulano', 'R$40.000,00', 'R$40.000,00', 'R$40.000,00','48x','124124214']} />
+        <TableBody value= {['', '', '', '','','']} />
+        <TableBody value= {['', '', '', '','','']} />
+        <TableBody value= {['', '', '', '','','']} />
+        <TableBody value= {['', '', '', '','','']} />
+        <TableBody value= {['', '', '', '','','']} />
       </table>
     </>
   );
--- src/feed.js
+++ src/feed.js
@@ -2,8 +2,22 @@ import { Table } from "./components/table/Table";
 import { Overrall } from "./components/overrall/Overrall.js";
 import { Header } from "./components/header/Header.js";
 import { Footer } from './components/footer/Footer.js';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+    useEffect(()=> {
+          axios.get('https://mocki.io/v1/5fcf051d-2867-4698-8c1c-df076b4d2948') 
+          .then((response)=>{
+            console.log(response.data.cessao)
+            setPosts(response.data.detalhes);
+          })
+          .catch(()=> {
+            console.log("ERRRRRRRROU")
+          })
+        }, [])
 
   return (
     <>
@@ -27,6 +41,28 @@ const Feed = () => {
         <section className="tableContainer">
           <Table />
         </section>
+
+        <div>
+        
+        {posts.map((post) => {
+          return (
+            <div>
+              <h2 className="post-body">{post.nomeCompleto}</h2>
+              <h2 className="post-title">{post.cpf}</h2>
+              <p className="post-body">{post.cet}</p>
+              <p className="post-body">{post.id}</p>
+              <p className="post-body">{post.numCcb}</p>
+              <p className="post-body">{post.tipo}</p>
+              <p className="post-body">{post.parcelas}</p>
+              <p className="post-body">{post.valorAquisicao}</p>
+              <p className="post-body">{post.valorBruto}</p>
+              <p className="post-body">{post.valorFinanciado}</p>
+            </div>
+          );
+        }
+        )}
+      </div>
+      
       </main>
       <Footer />
     </>
