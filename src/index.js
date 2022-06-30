557e7f73718cecf9f70507f2efa5d40f45f49db3
loulima <louizalima13@gmail.com>
2022-06-30 01:54:48 -0300

    media XS max 575px

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './feed.css';
+import '../src/pages/Feed/feed.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
